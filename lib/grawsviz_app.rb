29c8e7924cae0b0540338e86afe9b6ca160abebe
dacamp <dcampbell@nestlabs.com>
2013-05-13 17:05:01 -0700

    Added ability to view it from your local machine.  Pretty hacky still, but it's a start

---
+++ lib/grawsviz_app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
+require 'grawsviz'
+
+@access_key = ARGV[0] || ENV['AWS_ACCESS_KEY_ID']
+@secret_key = ARGV[1] || ENV['AWS_SECRET_ACCESS_KEY']
+
+get '/' do
+  @gr = GrAWSViz.new(@access_key, @secret_key)
+  @gr.generate_graph
+  redirect to("/view?file=#{@gr.file_name}")
+end
+
+get '/view' do
+  type="image/svg+xml"
+  File.read(File.join(File.expand_path(File.dirname(__FILE__)) << "/../", params[:file]))
+end
+
+get '/node/:n' do
+  @gr = GrAWSViz.new(@access_key, @secret_key, {:group_names => [params[:n]].flatten})
+  @gr.generate_graph
+  redirect to("/view?file=#{@gr.file_name}")
+end
+
