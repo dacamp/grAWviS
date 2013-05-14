2ce503369f6d26d58f85c1b5cd8f5215b4779fb9
dacamp <dcampbell@nestlabs.com>
2013-05-13 17:15:17 -0700

    type

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,15 +1,15 @@
 KNOWN_NAMES = {
   '0.0.0.0/0'        => 'Internet',
-  'xxxxxxxxxx'       => 'AWS Acount',
+  'xxxxxxxxxx'       => 'AWS Account',
   #  'x.x.x.x/0' => 'External',
   #  'y.y.y.y/0' => 'External',
 }
 
 KNOWN_STYLES =  lambda do
   hash = {
-    'AWS Acount' => { :color => 'lightblue' },
-    'Internet'   => { :color => 'orange'},
-    'amazon-elb' => { :color => 'yellow'},
+    'AWS Account' => { :color => 'lightblue' },
+    'Internet'    => { :color => 'orange'},
+    'amazon-elb'  => { :color => 'yellow'},
   }
   hash.default = { :color => 'red' }
   hash
